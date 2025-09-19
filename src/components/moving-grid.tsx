2a3f4ae1e99eb95b68c0fb276a9131fe342fee27
Diseno Divino <disenodivino.nmit@gmail.com>
2025-09-19 14:37:49 +0000

    animate the grid for continuous grid scrolling

--- src/components/moving-grid.tsx
+++ src/components/moving-grid.tsx
@@ -12,10 +12,8 @@ const MovingGrid = () => {
           "bg-[linear-gradient(to_right,hsl(var(--border))_1px,transparent_1px),linear-gradient(to_bottom,hsl(var(--border))_1px,transparent_1px)]",
           "bg-[size:2rem_2rem]",
           "[mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]",
+          "animate-grid-pan"
         )}
-        style={{
-            animation: "grid-pan 15s linear infinite",
-        }}
       ></div>
     </div>
   );
